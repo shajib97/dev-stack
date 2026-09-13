f008036441a597900df02c2876e3b0a5bdb0aa31
shajib97 <prantabhattacherjee@gmail.com>
2026-09-13 17:56:50 +0600

    Add: Toastify animation added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,32 +3,37 @@ import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import TechnologyList from "../components/TechnologyList";
 import Stack from "../components/Stack";
+import { ToastContainer, toast } from "react-toastify";
 
 const Home = () => {
   const [stack, setStack] = useState([]);
 
   const handleAddToStack = (technology) => {
-    const alreadyAdded = stack.find(
-      (item) => item.id === technology.id
-    );
+    const alreadyAdded = stack.find((item) => item.id === technology.id);
 
     if (alreadyAdded) {
+      toast.error(`${technology.name} is already in your stack!`);
       return;
     }
 
     setStack([...stack, technology]);
+
+    toast.success(`${technology.name} added to your stack`);
   };
 
   const handleRemove = (id) => {
-    const remainingStack = stack.filter(
-      (technology) => technology.id !== id
-    );
+    const technology = stack.find((item) => item.id === id);
+
+    const remainingStack = stack.filter((item) => item.id !== id);
 
     setStack(remainingStack);
+
+    toast.info(`${technology.name} removed from your stack`);
   };
 
   const handleRemoveAll = () => {
     setStack([]);
+    toast.info("All technologies removed");
   };
 
   return (
@@ -39,12 +44,9 @@ const Home = () => {
 
       <main className="max-w-7xl mx-auto px-4 md:px-8 py-16">
         <div className="flex flex-col lg:flex-row gap-8">
-
           {/* Technology Section */}
           <div className="flex-1">
-            <TechnologyList
-              handleAddToStack={handleAddToStack}
-            />
+            <TechnologyList handleAddToStack={handleAddToStack} />
           </div>
 
           {/* Stack Section */}
@@ -55,9 +57,9 @@ const Home = () => {
               handleRemoveAll={handleRemoveAll}
             />
           </div>
-
         </div>
       </main>
+      <ToastContainer position="top-right" />
     </>
   );
 };
